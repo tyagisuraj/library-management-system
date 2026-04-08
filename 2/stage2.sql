98cb937c62bf1f414d8736d0f5f67ef180f6b879
Suraj Tyagi <tyagisuraj678@gmail.com>
2026-04-08 13:12:59 +0530

    Update SQL queries in stage2.sql

--- 2/stage2.sql
+++ 2/stage2.sql
@@ -20,50 +20,50 @@ Issue ⨝ Fine
 10. Get total fine amount (aggregation)
 γ SUM(amount)(Fine)"""
 """🔷 PART 2: SQL Queries (10)"""
-1. Get all students
+---1. Get all students
 SELECT * FROM Student;
-2. Get students of a specific course
+---2. Get students of a specific course
 SELECT * FROM Student
 WHERE course = 'BTech';
-3. Get all books by a specific author
+---3. Get all books by a specific author
 SELECT * FROM Book
 WHERE author = 'XYZ';
-4. Get issued books with student details
+---4. Get issued books with student details
 SELECT S.name, I.issue_date, I.return_date
 FROM Student S
 JOIN Issue I ON S.student_id = I.student_id;
-5. Get student names and book titles issued
+---5. Get student names and book titles issued
 SELECT S.name, B.title
 FROM Student S
 JOIN Issue I ON S.student_id = I.student_id
 JOIN Book B ON I.book_id = B.book_id;
-6. Get students who have not issued any book
+---6. Get students who have not issued any book
 SELECT student_id, name
 FROM Student
 WHERE student_id NOT IN (SELECT student_id FROM Issue);
-7. Get currently issued books
+---7. Get currently issued books
 SELECT DISTINCT book_id
 FROM Issue;
-8. Get fine details with issue info
+---8. Get fine details with issue info
 SELECT F.amount, F.days_late, I.issue_date
 FROM Fine F
 JOIN Issue I ON F.issue_id = I.issue_id;
-9. Get students who returned books late
+---9. Get students who returned books late
 SELECT DISTINCT S.name
 FROM Student S
 JOIN Issue I ON S.student_id = I.student_id
 JOIN Fine F ON I.issue_id = F.issue_id
 WHERE F.days_late > 0;
-10. Get total fine collected
+---10. Get total fine collected
 SELECT SUM(amount) AS Total_Fine
 FROM Fine;
-11. Most issued book
+--11. Most issued book
 SELECT book_id, COUNT(*) AS issue_count
 FROM Issue
 GROUP BY book_id
 ORDER BY issue_count DESC
 LIMIT 1;
-12. Students with highest fines
+---12. Students with highest fines
 SELECT S.name, SUM(F.amount) AS total_fine
 FROM Student S
 JOIN Issue I ON S.student_id = I.student_id
